dc2d143ddcd0bcee083c42ba3f436da87faa05e1
aondrej ryan <aondrejryan11@gmail.com>
2024-06-12 23:15:30 -0700

    im working on something

---
+++ frontend/main.js
@@ -0,0 +1,8 @@
+const hamMenu = document.querySelector('#ham-menu')
+const offScreenMenu = document.querySelector('.off-screen-menu')
+
+hamMenu.addEventListener('click', () => {
+    console.log('hey does this work')
+    hamMenu.classList.toggle('active')
+    offScreenMenu.classList.toggle('active')
+})
